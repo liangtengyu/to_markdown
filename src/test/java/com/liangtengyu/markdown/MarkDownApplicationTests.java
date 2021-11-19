19ff221aec0c58b228c7e787a6cd6b3a89c6abe8
lty <843328437@qq.com>
2021-11-19 15:35:13 +0800

    修复一系列问题~

--- src/test/java/com/liangtengyu/markdown/MarkDownApplicationTests.java
+++ src/test/java/com/liangtengyu/markdown/MarkDownApplicationTests.java
@@ -17,11 +17,6 @@ public class MarkDownApplicationTests {
 	SaveFileService saveFileService;
 
 	@Test
-	public void testsave() throws IOException {
-		String testsavefile = saveFileService.saveToFile("4:56:19.834 [main] DEBUG org.springframework.test.context.BootstrapUtils - Instantiating CacheAwareContextLoaderDelegate from class [org.springframework.test.context.cache.DefaultCacheAwareContextLoaderDelegate]\n" +
-				"14:56:19.839 [main] DEBUG org.springframework.test.context.BootstrapUtils - Instantiating BootstrapContext using constructor [public org.springframework.test.context.support.DefaultBootstrapContext(java.lang.Class,org.springframework.test.context.CacheAwareContextLoaderDelegate)]\n" +
-				"14:56:19.853 [main] DEBUG org.springframework.test.context.BootstrapUtils - Instantiating TestContextBootstrapper for test class [com.liangtengyu.markdown.MarkDownApplicationTests] from class [org.springframework.boot.test.context.SpringBootTestContextBootstrapper]\n" +
-				"14:56:19.861 [main] INFO org.springframework.boot.test.context.SpringBootTestContextBootstrapper - Neither @ContextConfiguration nor @ContextHierarchy found for test class [com.liangtengyu.markdown.MarkDownApplicationTests], using SpringBootContextLoader\n");
-	}
+	public void testsave() throws IOException { }
 
 }
